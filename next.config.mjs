a1a911cfe4284911a004f2e60ebb47eaa09df85d
aylanuchiha <louniciaylanpro@gmail.com>
2026-03-07 00:00:43 +0100

    rechargement automatique de la page normalement

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // <--- C'EST LA LIGNE MAGIQUE MANQUANTE
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
+  basePath: '/carplay', // <--- INDISPENSABLE pour que GitHub trouve tes images et ton CSS
 }
 
 export default nextConfig
